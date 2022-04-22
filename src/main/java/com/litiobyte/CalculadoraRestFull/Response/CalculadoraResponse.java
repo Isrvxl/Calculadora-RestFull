184c0972227db77b75d904a3992364235750166f
Israel <isrvelverdugo@gmail.com>
2022-04-22 15:14:41 -0400

    create calculadoraResponse

---
+++ src/main/java/com/litiobyte/CalculadoraRestFull/Response/CalculadoraResponse.java
@@ -0,0 +1,18 @@
+package com.litiobyte.CalculadoraRestFull.Response;
+
+public class CalculadoraResponse {
+    
+    private Double resultado;
+
+    public CalculadoraResponse() {
+    }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
+
+}
