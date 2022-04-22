c7a992c760beccd7c7631273af736896ad9687f4
Israel <isrvelverdugo@gmail.com>
2022-04-22 15:11:38 -0400

    first commit

---
+++ src/main/java/com/litiobyte/CalculadoraRestFull/CalculadoraRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.litiobyte.CalculadoraRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestFullApplication.class, args);
+	}
+
+}
