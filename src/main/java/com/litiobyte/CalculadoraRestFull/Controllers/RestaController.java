2ab92c4e20273bdf74fe1c90d639602b8d6c4fa1
Israel <isrvelverdugo@gmail.com>
2022-04-22 15:19:18 -0400

    create restaController

---
+++ src/main/java/com/litiobyte/CalculadoraRestFull/Controllers/RestaController.java
@@ -0,0 +1,23 @@
+package com.litiobyte.CalculadoraRestFull.Controllers;
+
+import com.litiobyte.CalculadoraRestFull.Request.CalculadoraRequest;
+import com.litiobyte.CalculadoraRestFull.Response.CalculadoraResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadoraLitiobyte")
+public class RestaController {
+    
+    
+    @PostMapping("/restar")
+    public CalculadoraResponse calcular(@RequestBody CalculadoraRequest rq) {
+
+        CalculadoraResponse rs = new CalculadoraResponse();
+        rs.setResultado(0.0);
+        rs.setResultado(rq.getValor1() - rq.getValor2());
+        return rs;
+    } 
+}
